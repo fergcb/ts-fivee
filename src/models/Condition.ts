0c3589435d8290dee45677fb3cc9775d516050ad
Fergus Bentley <fergus.bentley@gmail.com>
2020-09-06 17:21:34 +0100

    Add name getter to Condition model

--- src/models/Condition.ts
+++ src/models/Condition.ts
@@ -3,6 +3,10 @@ import { ConditionData } from '../structures'
 
 export default class Condition extends Model<ConditionData> {
     
+    get name (): ConditionData['name'] {
+        return this.data.name
+    }
+
     get desc (): ConditionData['desc'] {
         return this.data.desc
     }
